a66358c341d026256bdd08beb4378bc5896b499a
- - <>
2025-02-24 16:48:17 +0300

    fix_5. Score is 100

--- tasks/tutorial/aplusb/aplusb.hpp
+++ tasks/tutorial/aplusb/aplusb.hpp
@@ -2,9 +2,11 @@
 
 int Sum(int a, int b) {
     int result = a + b;
-    if (result > std::numeric_limits<int>::max())
+    if (result > std::numeric_limits<int>::max()) {
         return std::numeric_limits<int>::max();
-    if (result < std::numeric_limits<int>::min())
+    }
+    if (result < std::numeric_limits<int>::min()) {
         return std::numeric_limits<int>::min();
+    }
     return result;
 }
