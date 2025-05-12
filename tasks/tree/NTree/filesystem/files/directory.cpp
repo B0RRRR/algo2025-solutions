1b5a42b5ee86a8af9fb91e883d4698b980031ca0
- - <>
2025-05-12 17:21:30 +0300

    FIX CI. Score is 200

--- tasks/tree/NTree/filesystem/files/directory.cpp
+++ tasks/tree/NTree/filesystem/files/directory.cpp
@@ -1,3 +1,20 @@
 #include "directory.hpp"
 
-namespace filesystem {}  // end namespace filesystem
+namespace filesystem {
+
+std::string Directory::GetName() const {
+    // fix
+    if (!parent_) {
+        return "/";
+    }
+
+    for (const auto& [name, child_ptr] : parent_->childs_.Values()) {
+        if (child_ptr == this) {
+            return name;
+        }
+    }
+
+    return {};
+}
+
+}  // end namespace filesystem
